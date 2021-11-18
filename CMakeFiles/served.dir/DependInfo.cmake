7fcbff48ec4d9ae650b460c5d4df5ec7b4c69d49
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2021-11-18 23:17:18 +0100

    updated to fit my needs

---
+++ CMakeFiles/served.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/methods_handler.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/methods_handler.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/multiplexer.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/multiplexer.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/mux/regex_matcher.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/mux/regex_matcher.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/mux/static_matcher.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/mux/static_matcher.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/mux/variable_matcher.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/mux/variable_matcher.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/net/connection.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/net/connection.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/net/connection_manager.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/net/connection_manager.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/net/server.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/net/server.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/parameters.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/parameters.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/plugins/access_log.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/plugins/access_log.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/request.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/request.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/request_parser.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/request_parser.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/request_parser_impl.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/request_parser_impl.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/response.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/response.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/status.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/status.cpp.o"
+  "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/uri.cpp" "/media/visual-rock/Extern4tb/Projects/GitRepos/zwoo/backend/lib/served/CMakeFiles/served.dir/uri.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/served/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/served.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "../../bin/libserved.a"
+  "../../bin/libserved.pdb"
+  "CMakeFiles/served.dir/methods_handler.cpp.o"
+  "CMakeFiles/served.dir/multiplexer.cpp.o"
+  "CMakeFiles/served.dir/mux/regex_matcher.cpp.o"
+  "CMakeFiles/served.dir/mux/static_matcher.cpp.o"
+  "CMakeFiles/served.dir/mux/variable_matcher.cpp.o"
+  "CMakeFiles/served.dir/net/connection.cpp.o"
+  "CMakeFiles/served.dir/net/connection_manager.cpp.o"
+  "CMakeFiles/served.dir/net/server.cpp.o"
+  "CMakeFiles/served.dir/parameters.cpp.o"
+  "CMakeFiles/served.dir/plugins/access_log.cpp.o"
+  "CMakeFiles/served.dir/request.cpp.o"
+  "CMakeFiles/served.dir/request_parser.cpp.o"
+  "CMakeFiles/served.dir/request_parser_impl.cpp.o"
+  "CMakeFiles/served.dir/response.cpp.o"
+  "CMakeFiles/served.dir/status.cpp.o"
+  "CMakeFiles/served.dir/uri.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/served.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
